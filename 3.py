be1f7e5b212973e7e1b2d02c39f08b9a10fca5be
sheyiqi <shiki.she@aliyun.com>
2017-03-29 23:06:34 +0800

    add functions

--- 3.py
+++ 3.py
@@ -1,43 +1,64 @@
+def trantotwo(a):
+    b=str()
+    a+='dd'
+    for i in range(len(a)-2):
+        if a[i]==a[i+1] and a[i]==a[i+2]:
+            pass
+        else:
+            b+=a[i]
+    return b
+
+def twotrantonone(a):
+    b=str()
+    a+='d'
+    for i in range(len(a)-1):
+        if a[i]==a[i+1] or a[i]== a[i-1]:
+            pass
+        else:
+            b+=a[i]
+    return b
+def isOne(a):
+    for i in range(len(a)-1):
+        if a[i]!=a[i+1]:
+            return False
+    return True
+            
+
+
 while True:
     try:
-        y=str()
-        z=str()
-        x=raw_input()
-        x+='dd'
-        for i in range(len(x)-2):
-            if x[i]==x[i+1] and x[i]==x[i+2]:
-                pass
-            else:
-                y+=x[i]
-        y+='d'
-        print (y)
-        for i in range(len(y)-1):
-            if y[i]==y[i+1] or y[i]==y[i-1]:
-                pass
-            else:
-                z+=y[i]
-        print (z)
-        if len(z)==0:
-            print (len(x)-1)
+        a=raw_input()
+        print (a)
+        y=trantotwo(a)
+        x=str[]
+        while (len(x)!=len(y)):
+            x=twotrantonone(y)
+            z=y
+            y=x
+            x=z
+        if len(x)==0:
+            print (len(a)+1)
         else:
+            z=x
             z+='dd'
             for i in range(len(z)-2):
                 if z[i]==z[i+2]:
-                    y=z[:i+1]+z[i+1]+z[i+1:]
-                    y+='d'
+                    y=z[:i+2]+z[i+1]+z[i+1:-1]
                     print (z)
                     print (y)
                     z=str()
-                    for i in range(len(y)-1):
-                        if y[i]==y[i+1] or y[i]==y[i-1]:
-                            pass
-                        else:
-                            z+=y[i]
-                    print (z)
-                    print (len(x)-len(z))
-                    break
-            print (z)
-            print (len(x)-len(z)+2)
+                    while (len(x)!=len(y)):
+                        x=twotrantonone(y)
+                        z=y
+                        y=x
+                        x=z
+                    if len(x)==0:
+                        print (len(a)+1)
+                    else:
+                        print (len(a)-len(z))
+                        break
+        print (z)
+        print (len(x)-len(z)+2)
 
     except EOFError:
         break
